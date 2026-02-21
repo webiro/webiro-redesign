f04c964e4ed5cf5c8bb6ae28808543b77124a24c
Pedram Gholizadeh <pedralizad@gmail.com>
2026-02-21 19:19:57 +0330

    add astro + svelte

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import svelte from '@astrojs/svelte';
+
+export default defineConfig({
+  site: 'https://webiro.github.io',
+  base: '/webiro-redesign',
+});
