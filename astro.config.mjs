b12e0348cf91559dce867a245e9f7e675eefe778
Pedram Gholizadeh <pedralizad@gmail.com>
2026-02-21 20:02:33 +0330

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import path from 'node:path';
 
 export default defineConfig({
   site: 'https://webiro.github.io',
   base: '/webiro-redesign',
+  integrations: [tailwind(), svelte()],
+  vite: {
+    resolve: {
+      alias: {
+        '@layouts': path.resolve('./src/layouts'),
+      },
+    },
+  },
 });
-
